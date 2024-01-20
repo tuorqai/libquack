168f7d08075e4b90f9f135fb25793c9bd76aa3ba
tuorqai <tuorqai@gmail.com>
2024-01-21 03:30:54 +0900

    graphics: add command buffer

--- src/graphics.c
+++ src/graphics.c
@@ -18,6 +18,7 @@
 // 3. This notice may not be removed or altered from any source distribution.
 //------------------------------------------------------------------------------
 
+#include <stb_ds.h>
 #include "graphics.h"
 #include "log.h"
 
@@ -33,9 +34,26 @@ static struct libqu_graphics_impl const *impl_list[] = {
 
 //------------------------------------------------------------------------------
 
+enum renderop
+{
+    RENDEROP_CLEAR,
+};
+
+struct rendercmd
+{
+    enum renderop op;
+
+    union {
+        struct {
+            qu_color color;
+        } clear;
+    } args;
+};
+
 static struct
 {
     struct libqu_graphics_impl const *impl;
+    struct rendercmd *rendercmds;
 } priv;
 
 //------------------------------------------------------------------------------
@@ -51,6 +69,17 @@ static struct libqu_graphics_impl const *choose_impl(void)
     }
 
     abort();
+}
+
+static void exec_cmd(struct rendercmd const *cmd)
+{
+    switch (cmd->op) {
+    case RENDEROP_CLEAR:
+        priv.impl->clear(cmd->args.clear.color);
+        break;
+    default:
+        break;
+    }
 }
 
 //------------------------------------------------------------------------------
@@ -69,17 +98,34 @@ void libqu_graphics_initialize(struct libqu_graphics_params const *params)
 
 void libqu_graphics_terminate(void)
 {
+    arrfree(priv.rendercmds);
     priv.impl->terminate();
+
+    memset(&priv, 0, sizeof(priv));
 
     LIBQU_LOGI("Terminated.\n");
 }
 
 void libqu_graphics_flush(void)
 {
+    for (size_t i = 0; i < arrlenu(priv.rendercmds); i++) {
+        exec_cmd(&priv.rendercmds[i]);
+    }
+
+    arrsetlen(priv.rendercmds, 0);
 }
 
 void libqu_graphics_clear(qu_color color)
 {
-    priv.impl->clear(color);
+    struct rendercmd cmd = {
+        .op = RENDEROP_CLEAR,
+        .args = {
+            .clear = {
+                .color = color,
+            },
+        },
+    };
+
+    arrput(priv.rendercmds, cmd);
 }
 
