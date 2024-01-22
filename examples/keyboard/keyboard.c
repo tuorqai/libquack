7d7c7ac5267786058579c35f5c5ee44ca44bc370
tuorqai <tuorqai@gmail.com>
2024-01-22 18:08:02 +0900

    update keyboard example

--- examples/keyboard/keyboard.c
+++ examples/keyboard/keyboard.c
@@ -1,39 +1,39 @@
 
 #include <libqu/libqu.h>
 
+static qu_color key_state_color(qu_key_state state)
+{
+    if (state == QU_KEY_STATE_PRESSED) {
+        return 0x606060FF;
+    } else if (state == QU_KEY_STATE_RELEASED) {
+        return 0x00E000FF;
+    }
+
+    return 0xF0F0F0FF;
+}
+
 int main(int argc, char *argv[])
 {
-    qu_vec2f pos = { 256.f, 256.f };
-    qu_vec2f size = { 32.f, 32.f };
-
     qu_set_window_title("[libquack] keyboard.c");
     qu_set_window_size(512, 512);
 
     qu_initialize();
 
     while (qu_process()) {
-        if (qu_is_key_pressed(QU_KEY_LEFT)) {
-            pos.x -= 1.f;
-        }
-
-        if (qu_is_key_pressed(QU_KEY_RIGHT)) {
-            pos.x += 1.f;
-        }
-    
-        if (qu_is_key_pressed(QU_KEY_UP)) {
-            pos.y -= 1.f;
-        }
-
-        if (qu_is_key_pressed(QU_KEY_DOWN)) {
-            pos.y += 1.f;
-        }
-
         qu_clear(0x202020FF);
 
-        float x = pos.x - size.x / 2.f;
-        float y = pos.y - size.y / 2.f;
+        qu_draw_rectangle(240.f, 220.f, 32.f, 32.f, 0xE00000FF,
+            key_state_color(qu_get_key_state(QU_KEY_UP)));
 
-        qu_draw_rectangle(x, y, size.x, size.y, 0xE00000FF, 0xF0F0F0FF);
+        qu_draw_rectangle(204.f, 260.f, 32.f, 32.f, 0xE00000FF,
+            key_state_color(qu_get_key_state(QU_KEY_LEFT)));
+
+        qu_draw_rectangle(240.f, 260.f, 32.f, 32.f, 0xE00000FF,
+            key_state_color(qu_get_key_state(QU_KEY_DOWN)));
+
+        qu_draw_rectangle(276.f, 260.f, 32.f, 32.f, 0xE00000FF,
+            key_state_color(qu_get_key_state(QU_KEY_RIGHT)));
+
         qu_present();
     }
 
