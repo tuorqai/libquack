7c729e1d7d059031e5eb8d34537d115f3f25a08a
tuorqai <tuorqai@gmail.com>
2024-02-04 15:01:48 +0900

    Rename public header from "libqu.h" to "libquack.h" so it matches the actual library name (#15)

--- src/fs.h
+++ src/fs.h
@@ -18,7 +18,7 @@
 // 3. This notice may not be removed or altered from any source distribution.
 //------------------------------------------------------------------------------
 
-#include "libqu/libqu.h"
+#include "libquack.h"
 
 //------------------------------------------------------------------------------
 
