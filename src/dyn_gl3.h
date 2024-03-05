8549d1d0bb2b785d548f6ddd4617f17d8fd45c78
tuorqai <tuorqai@gmail.com>
2024-03-05 18:46:57 +0900

    Include windows.h for OpenGL

--- src/dyn_gl3.h
+++ src/dyn_gl3.h
@@ -23,6 +23,11 @@
 
 //------------------------------------------------------------------------------
 
+#ifdef _WIN32
+    #define WIN32_LEAN_AND_MEAN
+    #include <windows.h>
+#endif
+
 #include <GL/gl.h>
 #include <GL/glext.h>
 #include "core.h"
